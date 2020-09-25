cf585da935d2da146709b2f8cc5f1f0def439b13
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-25 08:36:32 -0400

    editing gm.bat

--- gm.bat
+++ gm.bat
@@ -1,22 +1,24 @@
 @echo off
 
-set /p Option=Start morning sequence? (r - rewards/m - mail/s - school)? 
+echo Start morning sequence (r - rewards/m - mail/s - school)? 
 
-if %Option%==r (
+if %1==r (
     echo Opening Microsoft Rewards
     start chrome https://account.microsoft.com/rewards/
 ) 
 
-if %Option%==m (
+if %1==m (
     echo Opening Personal Email
     echo Opening School Email
     start chrome https://mail.google.com/mail/u/1/#inbox
     start chrome https://mail.google.com/mail/u/0/#inbox
 ) 
 
-if %Option%==s (
+if %1==s (
     echo Opening Schoology
     echo Opening StudentVue
+    echo Opening MasteringBio
     start chrome https://cpschools.schoology.com/
     start chrome https://synergyportal.cpschools.com/PXP2_Login.aspx
+    start chrome https://www.pearsonmylabandmastering.com/northamerica/masteringbiology/
 )
