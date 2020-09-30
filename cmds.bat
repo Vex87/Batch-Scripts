ae6ea3005a9a265c15fd915f018bb2c460a7e4b5
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-30 11:40:33 -0400

    formatted cmds & helpauto

--- cmds.bat
+++ cmds.bat
@@ -5,11 +5,11 @@ echo ---- // COMMAND PROMPT (CMD) \\ ----
 echo:
 echo CD ..                          Sets directory to parent.
 echo CD \                           Sets directory to CL.
-echo CD <LOCATION>                  Sets directory.
+echo CD [LOCATION]                  Sets directory.
 echo CLS                            Clear CL.
 echo CMD                            Starts a new instance of CL.
 echo DIR                            Return files of directory.
-echo ECHO <MSG>                     Returns message.
+echo ECHO [MSG]                     Returns message.
 echo EXIT                           Closes CL.
 echo NEWFILE                        Opens CL.
 echo RUNADMIN                       Starts a new instance of CL as an admin.
@@ -20,7 +20,7 @@ echo:
 echo ENDTASK                        Terminates a task.
 echo IPCONFIG                       Returns IP address.
 echo LOGOFF                         Logs the current user out of the machine.
-echo PING <URL>                     Allows the user to test the availability of a network connection to a specified host.
+echo PING [URL]                     Allows the user to test the availability of a network connection to a specified host.
 echo SYSTEMINFO                     Displays system info.
 echo TASKLIST                       Displays all currently running tasks including services.
 
@@ -38,42 +38,42 @@ echo:
 echo ---- // FOLDERS (FOL) \\ ----
 echo:
 echo DELFOL                         Deletes folder.
-echo MD <NAME>                      Creates folder.
+echo MD [NAME]                      Creates folder.
 echo OPEN                           Opens a folder.
 
 echo:
 echo ---- // FILES \\ ----
 echo:
 echo COMP                           Show differences between any files.
-echo DEL <NAME>                     Deletes file.
+echo DEL [NAME]                     Deletes file.
 echo FC                             Show differences between any files.
 echo NEWFILE                        Creates file.
-echo NEWTXTFILE <NAME>              Creates file with text.
-echo XCOPY <FILE> <DESTINATION>     Copy all descendants of path.
+echo NEWTXTFILE [NAME]              Creates file with text.
+echo XCOPY [FILE] [DESTINATION]     Copy all descendants of path.
 
 echo:
 echo ---- // FOLDERS AND FILES (FAF) \\ ----
 echo:
-echo COPY <FILE> <DESTINATION>      Copy file.
-echo MOVE <FILE> <DESTINATION>      Move file.
-echo REN <FILE> <NAME>              Renames file.
-echo START <FILE>                   Opens/runs folder/file.
+echo COPY [FILE] [DESTINATION]      Copy file.
+echo MOVE [FILE] [DESTINATION]      Move file.
+echo REN [FILE] [NAME]              Renames file.
+echo START [FILE]                   Opens/runs folder/file.
 
 echo:
 echo ---- // GOOGLE \\ ----
 echo:
 echo EXITCHROME                     Closes chromne.
-echo START CHROME <URL>             Search chrome.
+echo START CHROME [URL]             Search chrome.
 echo START CHROME --INCOGNITO       Starts chrome in incognito mode.
 
 echo:
 echo ---- // VISUAL STUDIO CODE (VSC) \\ ----
 echo:
-echo CODE -D <FILE> <FILE>          Compares two files.
+echo CODE -D [FILE] [FILE]          Compares two files.
 echo CODE -H                        Prints commands.
 echo CODE -NAME                     Opens new session.
 echo CODE .                         Open VSC.
-echo CODE <FILE>                    Opens new file.
+echo CODE [FILE]                    Opens new file.
 
 echo:
 echo ---- // AUTOMATIONS (AUTO) \\ ----
@@ -81,6 +81,8 @@ echo:
 echo GMM                            Stars morning sequence (email).
 echo GMR                            Stars morning sequence (rewards).
 echo GMS                            Stars morning sequence (school).
+echo USEHEADSET                     Changes the default playback device to the headset.
+echo USESPEAKERS                    Changes the default playback device to the speakers.
 
 echo:
 echo ---- // APPS \\ ----
--- helpauto.bat
+++ helpauto.bat
@@ -3,3 +3,5 @@
 echo GMM                            Stars morning sequence (email).
 echo GMR                            Stars morning sequence (rewards).
 echo GMS                            Stars morning sequence (school).
+echo USEHEADSET                     Changes the default playback device to the headset.
+echo USESPEAKERS                    Changes the default playback device to the speakers.
