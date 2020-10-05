a471481e3ce96a4e71e1955f255f3c187d52ceba
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-10-05 11:03:51 -0400

    updated gms

--- gms.bat
+++ gms.bat
@@ -5,6 +5,6 @@ echo Opening MasteringBio
 echo Opening Schoology
 echo Opening StudentVue
 start chrome https://apclassroom.collegeboard.org/6/assessments/assignments
-start chrome https://www.pearsonmylabandmastering.com/northamerica/masteringbiology/
+start chrome https://openvellum.ecollege.com/course.html?courseId=16305155&HepID=6ecc7b62a82e27338bf22bea5940e5b0#10001
 start chrome https://cpschools.schoology.com/home
 start chrome https://synergyportal.cpschools.com/PXP2_Login.aspx
