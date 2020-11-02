f04c2c34cdee11a7fcfb398996f1a5f0f3a33407
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-11-02 09:10:46 -0500

    added google calendar & habitica to gmm

--- gmm.bat
+++ gmm.bat
@@ -2,5 +2,9 @@
 
 echo Opening Personal Email in Gmail, Google
 echo Opening School Email in Gmail, Google
+echo Opening Google Calendar, Google
+echo Opening Habitica, Google
 start chrome https://mail.google.com/mail/u/1/#inbox
 start chrome https://mail.google.com/mail/u/0/#inbox
+start chrome https://calendar.google.com/calendar/u/0/r
+start chrome https://habitica.com/
