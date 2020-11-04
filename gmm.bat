1624fb351269efbb79fcf1202c8c08b6fb5f9f3c
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-11-04 09:02:03 -0500

    minor editsto gmm

--- gmm.bat
+++ gmm.bat
@@ -1,9 +1,9 @@
 @echo off
 
-echo Opening Personal Email in Gmail, Google
-echo Opening School Email in Gmail, Google
-echo Opening Google Calendar, Google
-echo Opening Habitica, Google
+echo Opening Personal Email in Gmail in Google
+echo Opening School Email in Gmail in Google
+echo Opening Google Calendar in Google
+echo Opening Habitica in Google
 start chrome https://mail.google.com/mail/u/1/#inbox
 start chrome https://mail.google.com/mail/u/0/#inbox
 start chrome https://calendar.google.com/calendar/u/0/r
