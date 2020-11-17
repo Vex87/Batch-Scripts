29dcf618337920c8d1c2bd0d4c995436cbaf675c
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-11-16 20:01:45 -0500

    updated help cmds & deleted open

--- cmds.bat
+++ cmds.bat
@@ -7,9 +7,7 @@ echo CD ..                          Sets directory to parent.
 echo CD \                           Sets directory to CL.
 echo CD [DIR]                       Sets directory.
 echo CLS                            Clear CL.
-echo CMD                            Starts a new instance of CL.
 echo DIR                            Return files of directory.
-echo ECHO [MSG]                     Returns message.
 echo EXIT                           Closes CL.
 echo NEW                            Opens CL.
 echo RUNADMIN                       Starts a new instance of CL as an admin.
@@ -19,50 +17,18 @@ echo ---- // PC \\ ----
 echo:
 echo ENDTASK                        Terminates a task.
 echo IPCONFIG                       Returns IP address.
-echo LOGOFF                         Logs the current user out of the machine.
 echo PING [URL]                     Allows the user to test the availability of a network connection to a specified host.
-echo SHUTDOWN                       Allows proper local or remote shutdown of machine.
 echo SYSTEMINFO                     Displays system info.
 echo TASKLIST                       Displays all currently running tasks including services.
 
 echo:
-echo ---- // MISC \\ ----
-echo:
-echo HIS                            View command history.
-echo SORT                           Insensitive case filtering.
-echo SYNC                           Sync local repo to system32 (Security: Admin).
-echo TREE                           Graphically displays the directory structure of apath.
-echo TYPE                           Displays the contents of a text file.
-
-echo:
 echo ---- // FILES \\ ----
 echo:
+echo HIS                            View command history.
 echo MD [NAME]                      Creates folder.
-echo OPEN                           Opens a folder.
-echo COMP                           Show differences between any files.
-echo DEL [NAME]                     Deletes file.
-echo FC                             Show differences between any files.
-echo XCOPY [FILE] [DESTINATION]     Copy all descendants of path.
-echo COPY [FILE] [DESTINATION]      Copy file.
-echo MOVE [FILE] [DESTINATION]      Move file.
-echo REN [FILE] [NAME]              Renames file.
 echo START [FILE]                   Opens/runs folder/file.
-
-echo:
-echo ---- // GOOGLE \\ ----
-echo:
-echo EXITCHROME                     Closes chromne.
-echo START CHROME [URL]             Search chrome.
-echo START CHROME --INCOGNITO       Starts chrome in incognito mode.
-
-echo:
-echo ---- // VISUAL STUDIO CODE \\ ----
-echo:
-echo CODE -D [FILE] [FILE]          Compares two files.
-echo CODE -H                        Prints commands.
-echo CODE -NAME                     Opens new session.
-echo CODE .                         Open VSC.
-echo CODE [FILE]                    Opens new file.
+echo SYNC                           Sync local repo to system32 (Security: Admin).
+echo TREE                           Graphically displays the directory structure of apath.
 
 echo:
 echo ---- // AUTOMATIONS \\ ----
--- open.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set /p DesiredLocation=Enter the location:
-%SystemRoot%\explorer.exe  %DesiredLocation%
-echo Opening %DesiredLocation%
